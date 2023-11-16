fddd4beb956a791699c0c2e565814d03f795e1c9
Yousef Abu Shanab <josephyousef249@gmail.com>
2023-11-16 00:16:54 -0800

    Add get_permissions method to OwnerMixin in mixins.py

--- mixins.py
+++ mixins.py
@@ -1,6 +1,9 @@
 """ Mixins for messenger """
 
 
+from messenger.permissions import IsOwner
+
+
 # Create your mixins here.
 class OwnerMixin:
     """Add the user field to a model automatically when creating"""
@@ -9,3 +12,9 @@ class OwnerMixin:
         """Save the model with request.user"""
 
         serializer.save(user=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ["update", "partial_update", "destroy"]:
+            self.permission_classes += [IsOwner]
+
+        return super().get_permissions()
